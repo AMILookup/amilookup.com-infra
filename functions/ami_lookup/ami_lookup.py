c28e5b63df74a1a205a9e9598952c7166ce945b5
Andrew Jackson <dorkmeisterx69@gmail.com>
2018-11-24 21:18:40 -0600

    fix ami_lookup function

--- functions/ami_lookup/ami_lookup.py
+++ functions/ami_lookup/ami_lookup.py
@@ -1,19 +1,7 @@
 import boto3
 import json
-from chalice import Chalice
-from chalice import CORSConfig
-cors_config = CORSConfig(
-    allow_origin='https://amilookup.com',
-)
-app = Chalice(app_name='amilookup')
-app.debug = True
 
-@app.route('/')
-def index():
-    print('Starting Hello World')
-    return {'Hello': 'World'}
-@app.route('/ami/{region}/{ami}', cors=cors_config, methods=['GET'])
-def ami(region, ami):
+def ami_lookup(region, ami):
     print('Starting Function')
     ec2 = boto3.resource('ec2', region_name=region)
     print('Looking for Image')
@@ -33,3 +21,9 @@ def ami(region, ami):
                         'image_type': image.image_type,
                         'platform': image.platform
                       })
+def lambda_handler(event, context):
+    print('Starting handler')
+    ami = event['query']['ami']
+    region = event['query']['region']
+    ami_lookup = ami_lookup(region, ami)
+    return ami_lookup
