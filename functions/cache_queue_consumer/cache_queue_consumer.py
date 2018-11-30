632253d8ddd5c7394c23c7b5dd55ea9083fa1437
Andrew Jackson <dorkmeisterx69@gmail.com>
2018-11-30 00:15:34 -0600

    major updates to consumer and feeder functions

--- functions/cache_queue_consumer/cache_queue_consumer.py
+++ functions/cache_queue_consumer/cache_queue_consumer.py
@@ -1,18 +1,60 @@
 import json
 import boto3
+import datetime
+from ami_lookup import ami_lookup
 
-queue = "https://sqs.us-east-1.amazonaws.com/124775650587/seed_queue"
-session = boto3.session.Session(profile_name='aws-dev-amilookup')
+ts = datetime.datetime.now()
+ts = str(ts)
 
+region = 'us-east-1'
 
-ec2 = session.resource('ec2', region_name='us-east-1')
-sqs = session.client('sqs', region_name='us-east-1')
-#image_iterator = ec2.images.all()
-image_iterator = ec2.images.all()
+sqs = boto3.client('sqs', region_name=region)
+dynamodb = boto3.resource('dynamodb')
 
-for i in image_iterator:
-    print(i.id)
-    response = sqs.send_message(
-        QueueUrl=queue,
-        MessageBody=i.id
-    )
+# Get the db
+table = dynamodb.Table('dev-amilookup-infra-cacheTable-1B4X4QTUCYRI0')
+
+def lambda_handler(event, context):
+    results = {}
+    for msg in event['Records']:
+
+        print(msg)
+        payload = {
+            'Id': msg['messageId'],
+            'ReceiptHandle': msg['receiptHandle'],
+            'ImageId': msg['body'],
+            'Region': msg['messageAttributes']['region']['stringValue']
+        }
+
+        print(payload)
+
+        ami_results = ami_lookup(payload['Region'],payload['ImageId'])
+        ami_results = json.loads(ami_results)
+
+        print(ami_results)
+
+        table.put_item(
+            Item={
+                "VirtualizationType": ami_results['VirtualizationType'],
+                "Description": str(ami_results['Description']),
+                "Hypervisor": ami_results['Hypervisor'],
+                "ImageOwnerAlias": ami_results['ImageOwnerAlias'],
+                "EnaSupport": ami_results['EnaSupport'],
+                "SriovNetSupport": ami_results['SriovNetSupport'],
+                "ImageId": ami_results['ImageId'],
+                "State": ami_results['State'],
+                "BlockDeviceMappings": ami_results['BlockDeviceMappings'],
+                "Architecture": ami_results['Architecture'],
+                "ImageLocation": ami_results['ImageLocation'],
+                "RootDeviceType": ami_results['RootDeviceType'],
+                "OwnerId": ami_results['OwnerId'],
+                "RootDeviceName": ami_results['RootDeviceName'],
+                "CreationDate": ami_results['CreationDate'],
+                "Public": ami_results['Public'],
+                "ImageType": ami_results['ImageType'],
+                "Name": ami_results['Name'],
+                "CreateTimestamp": ts
+            }
+        )
+
+    return {"message": "Batch processed."}
--- functions/cache_queue_feeder/cache_queue_feeder.py
+++ functions/cache_queue_feeder/cache_queue_feeder.py
@@ -3,10 +3,10 @@ import boto3
 
 queue = "https://sqs.us-east-1.amazonaws.com/124775650587/seed_queue"
 session = boto3.session.Session(profile_name='aws-dev-amilookup')
+region = 'us-east-1'
 
-
-ec2 = session.resource('ec2', region_name='us-east-1')
-sqs = session.client('sqs', region_name='us-east-1')
+ec2 = session.resource('ec2', region_name=region)
+sqs = session.client('sqs', region_name=region)
 #image_iterator = ec2.images.all()
 image_iterator = ec2.images.all()
 
@@ -14,5 +14,11 @@ for i in image_iterator:
     print(i.id)
     response = sqs.send_message(
         QueueUrl=queue,
-        MessageBody=i.id
+        MessageBody=i.id,
+        MessageAttributes={
+            'region': {
+                'StringValue': region,
+                'DataType': 'String'
+            }
+        }
     )
