7d5e024dbfa5abc95a430ef29fa741566bd6c61f
Andrew Jackson <dorkmeisterx69@gmail.com>
2018-12-24 12:34:33 -0600

    Update gitignore, add event.json for testing, update function for 500 code on malformed.

--- functions/ami_lookup/ami_lookup.py
+++ functions/ami_lookup/ami_lookup.py
@@ -1,5 +1,11 @@
 import boto3
+from botocore.exceptions import ClientError
 import json
+import sys
+
+def throws():
+    raise RuntimeError('The function failed for some reason.')
+
 
 def ami_lookup(region, ami):
     print('Starting Function')
@@ -7,11 +13,11 @@ def ami_lookup(region, ami):
     print('Looking for Image')
     print(ami)
     image = ec2.Image(ami)
-    print('Image found')
     image.load()
     print('Image loaded')
     print(image.name)
 
+    ami = {}
     ami = json.dumps({
         "VirtualizationType": image.virtualization_type,
         "Description": image.description,
@@ -33,6 +39,7 @@ def ami_lookup(region, ami):
         "Name": image.name
     })
 
+    # Handle Null values in JSON.
     ami_json = json.loads(ami)
     for key, value in ami_json.items():
         print("{} = {}".format(key, value))
@@ -50,8 +57,21 @@ def lambda_handler(event, context):
     response = {}
     ami = event['ami']
     region = event['region']
-    response = ami_lookup(region, ami)
-
+    try:
+        response = ami_lookup(region, ami)
+    except ClientError as e: 
+        if e.response['Error']['Code'] == 'InvalidAMIID.Malformed':
+            print("AMIId is Malformed")
+        return {
+            'isBase64Encoded': "false",
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'body': "AMIId is Malformed"
+    }
+    else:
+        print("Unexpected error: %s" % e)
     return {
         'isBase64Encoded': "false",
         'statusCode': 200,
